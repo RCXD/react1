7103d13cbee0bb8de5da84d5a125b5dfe017dc0f
tpxdkss <tpxdkss@gmail.com>
2025-07-17 14:06:43 +0900

    Add content support to board posts
    Introduced a new boardContent state to manage post contents alongside titles. Updated the post creation form to accept both title and content, and modified the Detail component to display the content of the selected post. Also added delete functionality to remove posts and their associated content.

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,13 @@ function App() {
   // const [변수명, 변경함수]=useState(초기값);
   const [title, setTitle] = useState('상품목록')
   const [boardTitle, setBoardTitle] = useState(['React', 'HTML', 'CSS'])
+  const [boardContent, setBoardContent] = useState(['React is...', 'HTML is...', 'CSS is...'])
   const [like, setLike] = useState([0, 0, 0])
   const [show, setShow] = useState(false)
   const [titleIdx, setTitleIdx]=useState(0)
+  const [newTitle, setNewTitle]=useState('')
+  const [newContent, setNewContent]=useState('')
+  //위 변수들을 객체로 만들어서 개선
 
   function showContent(idx){
     setShow(!show);
@@ -55,20 +59,84 @@ function App() {
                 setLike([...like.slice(0, i), like[i]+1, ...like.slice(i+1)])
               }}>좋아요</button> {like[i]} </h4>
               <p>2025-07-16</p>
+              <button onClick={()=>{
+                boardTitle.splice(i, 1)
+                boardContent.splice(i, 1)
+                like.splice(i, 1)
+                setBoardTitle([...boardTitle])
+                setLike([...like])
+              }}>삭제</button>
             </div>
           )
         })
       }
 
-      <button onClick={() => {
+      {/* <button onClick={() => {
         setBoardTitle(['Java', ...boardTitle.slice(1)])
-      }}>첫번째 게시물 제목 바꾸기</button>
+      }}>첫번째 게시물 제목 바꾸기</button> */}
       {/* <Detail 변수명=보낼값 /> 이렇게 자식으로 데이터를 보낼 수 있다. */}
       {/* {<Detail num={1}/>} */}
       
       {/* 컴포넌트 태그를 사용하면 부모자식관계 성립 */}
-      {show ? <Detail boardTitle={boardTitle} setBoardTitle={setBoardTitle} titleIdx={titleIdx} color="grey" like={like} test={test}/> : null} 
+      {show ? <Detail
+        boardTitle={boardTitle} 
+        setBoardTitle={setBoardTitle}
+        titleIdx={titleIdx} 
+        color="grey"
+        like={like}
+        test={test}
+        boardContent={boardContent}
+      /> : null} 
       {/* {show && <Detail />} */}
+      <br/>
+      {/* 쓰자마자 반영되게 하고싶으면 onInput. 다 쓰고 포커스를 텍스트박스 밖으로 뺄 떼는 onChange */}
+      {/* 아래는 되긴 되는데 다소 불편하네
+      <input type='text' onFocus={(e)=>{
+        e.target.value=''
+      }} onBlur={(e)=>{
+        console.log(e.target.value)
+        setNewTitle(e.target.value)
+      }}/><br/>
+      <textarea></textarea><br/>
+      <button onClick={()=>{
+        if (newTitle===''){
+          alert('제목을 입력하세요.')
+          return
+        }
+        // boardTitle.unshift(newTitle) //앞에 추가
+        // like.unshift(0)
+        boardTitle.push(newTitle) //뒤에 추가
+        like.push(0)
+        setLike([...like])
+        setBoardTitle([...boardTitle])
+        setNewTitle('')
+      }}>글작성</button> */}
+      <input type='text' value={newTitle} onChange={(e)=>{
+        setNewTitle(e.target.value)
+      }}/><br/>
+      <textarea value={newContent} onChange={(e)=>{
+        setNewContent(e.target.value)
+      }}></textarea><br/>
+      <button onClick={()=>{
+        if (newTitle===''){
+          alert('제목을 입력하세요.')
+          return
+        }
+        if (newContent===''){
+          alert ('내용을 입력하세요.')
+          return
+        }
+        // boardTitle.unshift(newTitle) //앞에 추가
+        // like.unshift(0)
+        boardTitle.push(newTitle) //뒤에 추가
+        boardContent.push(newContent)
+        like.push(0)
+        setLike([...like])
+        setBoardTitle([...boardTitle])
+        setBoardContent([...boardContent])
+        setNewTitle('')
+        setNewContent('')
+      }}>글작성</button>
     </div>
   )
 }
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import boardTitle from './App.jsx'
 
 // 부모로부터 받은 값을 받을 때, fundtion Detail(props) {}라고 매개변수를 활용한다.
@@ -10,15 +11,13 @@ import boardTitle from './App.jsx'
 // const color=props.color
 // const test=props.test
 //<매개변수와 선언문 통합/Destructuring & allocation>
-function Detail({boardTitle, setBoardTitle, titleIdx, color, test}){
-// const [boardTitle, setBoardTitle]
+function Detail({boardTitle, setBoardTitle, titleIdx, color, test, boardContent}){
+  // const [boardTitle, setBoardTitle]
   //  = useState(['React', 'HTML', 'CSS']);
   //변수만 받아올 경우 자식에서 직접 setter를 정의해줄 수 있다.
   //순환참조는 안되니 필요한 변수는 최상위 부모컴포넌트에서 정의하고 작업하는 것이 좋다
   //미리 고려하지 않으면 갈아엎는 일이 생길 수 있음>다만 공유를 도와주는 라이브러리가 있긴 함
   //자식>부모로 보낼 방법은 없다.
-
-
   return (
     <div className="detail" color={color}>
       <button onClick={()=>{
@@ -31,7 +30,7 @@ function Detail({boardTitle, setBoardTitle, titleIdx, color, test}){
       <p onClick={()=>{
         test()
       }}>날짜</p>
-      <p>내용</p>
+      <p>{boardContent[titleIdx]}</p>
     </div>
   )
 }
